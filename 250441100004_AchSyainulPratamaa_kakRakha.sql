c66de6ce2e592029d41b705f121b3ac8c80ff98c
syainulpratamal-crypto <syainulpratamal@gmail.com>
2026-04-01 15:20:19 +0700

    250441100004_AchSyainulPratana_Modul1_KakRakha

---
+++ 250441100004_AchSyainulPratamaa_kakRakha.sql
@@ -0,0 +1,28 @@
+USE eperpustakaan;
+CREATE TABLE koleksi_langka (
+    id_buku BIGINT PRIMARY KEY,
+    judul VARCHAR(250),
+    tahun_terbit YEAR,
+    estimasi_harga DECIMAL(15,2)
+);
+
+DESCRIBE koleksi_langka
+CREATE TABLE pelanggan_setia (
+nomor_identitas INT
+);
+
+ALTER TABLE pelanggan,
+MODIFY nomor_identitas VARCHAR(20);
+ DESCRIBE pelanggan_setia
+CREATE TABLE transaksi_harian (
+    id INT,
+    total DECIMAL(10,2)
+);
+DROP TABLE transaksi_harian;
+TRUNCATE TABLE transaksi_harian;
+
+CREATE TABLE umpan_balik (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    isi_komentar TEXT
+);
+DESCRIBE umpan_balik
